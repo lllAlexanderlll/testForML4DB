bab199a322d28c802e6313f13f960daf257d414f
lllAlexanderlll <alex.a.weber@gmx.de>
2020-08-17 15:34:05 +0200

    @

--- estimator/estimator.py
+++ estimator/estimator.py
@@ -35,18 +35,18 @@ class Estimator:
         It must contain at least the fields "loss_function", "dropout", "learning_rate", "kernel_initializer",
         "activation_strategy" and "layer".
 
-        :param config: Only used if neither a model or a model_path is passed.
+        @param config: Only used if neither a model or a model_path is passed.
             if given: It must contain at least the fields "loss_function", "dropout", "learning_rate",
             "kernel_initializer", "activation_strategy" and "layer".
             if not given: the config file 'config.yaml' is used for these settings.
-        :param config_file_path: path for the config-file -> only necessary if no config is given
-        :param data: Optional parameter for giving the data for training and testing. If given it has to be a Dict with
+        @param config_file_path: path for the config-file -> only necessary if no config is given
+        @param data: Optional parameter for giving the data for training and testing. If given it has to be a Dict with
             at least "x" and "y" and optionally "postgres_estimate" as keys. The values have to be numpy.ndarray. For
             key "x" it should be the vectorized queries, for key "y" the true cardinalities in the same order and for
             optional key "postgres_estimate" the estimates of the postgres optimizer for the query.
-        :param model: Option to pass a Model which can be used.
-        :param model_path: Option to pass a path to a saved model in an .h5 file.
-        :param debug: Boolean whether to print additional information while processing.
+        @param model: Option to pass a Model which can be used.
+        @param model_path: Option to pass a path to a saved model in an .h5 file.
+        @param debug: Boolean whether to print additional information while processing.
         """
 
         if model:
@@ -80,9 +80,9 @@ class Estimator:
         """
         Function for creating the model of the neural network with the information from self.config
 
-        :param len_input: The size of the input vector.
-        :param override: Whether an existing model should be overridden.
-        :return: The model for the neural network with the given properties.
+        @param len_input: The size of the input vector.
+        @param override: Whether an existing model should be overridden.
+        @return The model for the neural network with the given properties.
         """
 
         if self.model and not override:
@@ -116,7 +116,7 @@ class Estimator:
         """
         Method for loading an already existing model wich was saved to file.
 
-        :param model_path: Path to the file containing the model to load
+        @param model_path: Path to the file containing the model to load
         """
 
         self.model = load_model(model_path)
@@ -124,10 +124,10 @@ class Estimator:
     @staticmethod
     def denormalize(y, y_min: float, y_max: float):
         """
-        :param y: tensor filled with values to denormalize
-        :param y_min: minimum value for y
-        :param y_max: maximum value for y
-        :return: tensor with denormalized values
+        @param y: tensor filled with values to denormalize
+        @param y_min: minimum value for y
+        @param y_max: maximum value for y
+        @return tensor with denormalized values
         """
 
         return K.exp(y * (y_max - y_min) + y_min)
@@ -135,10 +135,10 @@ class Estimator:
     @staticmethod
     def denormalize_np(y: np.ndarray, y_min: float, y_max: float) -> np.ndarray:
         """
-        :param y: numpy-array filled with values to denormalize
-        :param y_min: minimum value for y
-        :param y_max: maximum value for y
-        :return: numpy-array with denormalized values
+        @param y: numpy-array filled with values to denormalize
+        @param y_min: minimum value for y
+        @param y_max: maximum value for y
+        @return numpy-array with denormalized values
         """
 
         return np.exp(y * (y_max - y_min) + y_min)
@@ -159,9 +159,9 @@ class Estimator:
         """
         Method for loading the data from file.
 
-        :param file_path: Path for the file where the data is stored. Has to be a .csv or .npy file.
-        :param override: Boolean whether to override already existing data.
-        :return: The data which is set for the Estimator.
+        @param file_path: Path for the file where the data is stored. Has to be a .csv or .npy file.
+        @param override: Boolean whether to override already existing data.
+        @return The data which is set for the Estimator.
         """
 
         if self.data is not None and not override and "x" in self.data and self.data[
@@ -186,8 +186,8 @@ class Estimator:
         """
         Method for setting data and dependent values like max_card and input_length.
 
-        :param loaded_data: The data loaded from the file.
-        :param override: Boolean whether to override already existing data.
+        @param loaded_data: The data loaded from the file.
+        @param override: Boolean whether to override already existing data.
         """
 
         if self.data and not override and "x" in self.data and self.data["x"] is not None and "y" in self.data and \
@@ -220,7 +220,7 @@ class Estimator:
         """
         Function to split the data into training- and test-set by a parameterized split value.
 
-        :param split: Percentage of the data going into training set. (split=0.9 means 90% of data is training set)
+        @param split: Percentage of the data going into training set. (split=0.9 means 90% of data is training set)
         """
 
         if self.data is None or "x" not in self.data or self.data["x"] is None or "y" not in self.data or \
@@ -246,16 +246,16 @@ class Estimator:
         """
         Method for training the before created Model.
 
-        :param epochs: Number of epochs for training.
-        :param verbose: How much information to print while training. 0 = silent, 1 = progress bar, 2 = one line per
+        @param epochs: Number of epochs for training.
+        @param verbose: How much information to print while training. 0 = silent, 1 = progress bar, 2 = one line per
             epoch.
-        :param shuffle: Whether to shuffle the training data -> not necessary if split was done by numpy.random.choice()
-        :param batch_size: Size for the batches -> Smaller batches may be able to train the neural network better
+        @param shuffle: Whether to shuffle the training data -> not necessary if split was done by numpy.random.choice()
+        @param batch_size: Size for the batches -> Smaller batches may be able to train the neural network better
             (possibly) but enlarge training time, while bigger batches may lead to a less well trained network while
             training faster.
-        :param validation_split: How much of the data should be taken as validation set -> these are taken from the
+        @param validation_split: How much of the data should be taken as validation set -> these are taken from the
             training data, not the test data, and are reselected for every epoch.
-        :return: Training history as dict.
+        @return Training history as dict.
         """
 
         if "x" not in self.training_data or self.training_data["x"] is None or "y" not in self.training_data or \
@@ -272,7 +272,7 @@ class Estimator:
         """
         Let the trained neural network predict the test data.
 
-        :return: numpy-array containing the normalized predictions of the neural network for the test data
+        @return numpy-array containing the normalized predictions of the neural network for the test data
         """
 
         if "x" not in self.test_data or self.test_data["x"] is None or "y" not in self.test_data or \
@@ -288,8 +288,8 @@ class Estimator:
         """
         Let the trained neural network predict the given data.
 
-        :param data: numpy-array containing at least one vectorized query which should be predicted
-        :return: numpy-array containing the normalized predictions of the neural network for the given data
+        @param data: numpy-array containing at least one vectorized query which should be predicted
+        @return numpy-array containing the normalized predictions of the neural network for the given data
         """
 
         if not self.model:
@@ -303,21 +303,21 @@ class Estimator:
         """
         Method for a full run of the Estimator, with training and testing.
 
-        :param data_file_path: Optional path to saved data file. Only necessary if no data has been set before.
-        :param epochs: Number of epochs for training.
-        :param verbose: How much information to print while training. 0 = silent, 1 = progress bar, 2 = one line per
+        @param data_file_path: Optional path to saved data file. Only necessary if no data has been set before.
+        @param epochs: Number of epochs for training.
+        @param verbose: How much information to print while training. 0 = silent, 1 = progress bar, 2 = one line per
             epoch.
-        :param shuffle: Whether to shuffle the training data -> not necessary if split was done by numpy.random.choice()
-        :param batch_size: Size for the batches -> Smaller batches may be able to train the neural network better
+        @param shuffle: Whether to shuffle the training data -> not necessary if split was done by numpy.random.choice()
+        @param batch_size: Size for the batches -> Smaller batches may be able to train the neural network better
             (possibly) but enlarge training time, while bigger batches may lead to a less well trained network while
             training faster.
-        :param validation_split: How much of the data should be taken as validation set -> these are taken from the
+        @param validation_split: How much of the data should be taken as validation set -> these are taken from the
             training data, not the test data, and are reselected for every epoch.
-        :param override_model: Whether to override a probably already existing model.
-        :param save_model: Whether to save the trained model to file.
-        :param save_model_file_path: When save_model==True this parameter is required to give the path where the model
+        @param override_model: Whether to override a probably already existing model.
+        @param save_model: Whether to save the trained model to file.
+        @param save_model_file_path: When save_model==True this parameter is required to give the path where the model
             should be saved.
-        :return: A numpy.ndarray containing the calculated q-error.
+        @return A numpy.ndarray containing the calculated q-error.
         """
 
         if data_file_path is not None:
@@ -339,7 +339,7 @@ class Estimator:
         """
         Method for saving the Model to file.
 
-        :param filename: Name of the file where the model should be stored. (Without file ending. ".h5" is added to the
+        @param filename: Name of the file where the model should be stored. (Without file ending. ".h5" is added to the
             filename)
         """
 
