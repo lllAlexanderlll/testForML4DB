7b632b05f04f891492d66376acfc1976f0e51be6
lllAlexanderlll <alex.a.weber@gmx.de>
2020-08-17 15:19:06 +0200

    asd

--- main.py
+++ main.py
@@ -96,3 +96,4 @@ if __name__ == "__main__":
     estimate(os.path.join(wd, "vectorizer_results"), os.path.join("estimator", "config.yaml"), 
                           os.path.join(wd, "model"))
 
+
